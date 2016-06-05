c7d29461a5b209047245f6b89ccc91e867cca052
Yan Yi Goh <yanyi@users.noreply.github.com>
2016-06-05 22:40:36 +0800

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -8,16 +8,23 @@ brew update
 # Upgrade any already-installed formulae
 brew upgrade --all
 
-# brew-cask
+# brew-cask daily
 brew cask install alfred
-brew cask install appcleaner
 brew cask install caffeine
 brew cask install dropbox
 brew cask install google-chrome
 brew cask install vlc
 
+# brew-cask less often
+brew cask install appcleaner
+brew cask install firefox
+brew cask install handbrake
+brew cask install slack
+
 # Install useful binaries
+brew install curl
 brew install git
+brew install git-flow
 
 # Remove outdated versions from the cellar
 brew cleanup
