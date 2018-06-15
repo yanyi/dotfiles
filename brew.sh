9f0f8d771cc4d356a8be88f7881799384ecd41c2
Yan Yi Goh <yy.git@protonmail.com>
2018-06-15 13:14:12 +0800

    brew.sh: Add more applications with cask and add more binaries
    · Added more applications with cask installation.
    · Removed VLC.
    · Added additional brew binaries

--- brew.sh
+++ brew.sh
@@ -10,22 +10,50 @@ brew upgrade --all
 
 # brew-cask daily
 brew cask install alfred
+brew cask install avast-security
 brew cask install caffeine
 brew cask install dropbox
 brew cask install firefox
-brew cask install vlc
+brew cask install iterm2
+brew cask install sourcetree
+brew cask install spotify
+brew cask install telegram
+brew cask install the-unarchiver
+brew cask install visual-studio-code
+brew cask install whatsapp
 
 # brew-cask less often
+brew cask install anki
 brew cask install appcleaner
+brew cask install calibre
+brew cask install cryptomator
+brew cask install discord
+brew cask install freefilesync
 brew cask install google-chrome
 brew cask install handbrake
+brew cask install iina
+brew cask install imageoptim
+brew cask install keybase
+brew cask install launchcontrol
+brew cask install mountain-duck
+brew cask install mpv
+brew cask install namechanger
+brew cask install postman
+brew cask install signal
+brew cask install skitch
+brew cask install time-lapse-assembler
+brew cask install transmission
+brew cask install veracrypt
+brew cask install waterfox
 
 # Install useful binaries
 brew install ffmpeg
 brew install git
+brew install git-crypt
 brew install git-flow
 brew install openssl
 brew install wget
+brew install youtube-dl
 brew install zsh
 
 # Remove outdated versions from the cellar
