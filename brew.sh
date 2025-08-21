2d222a8015c4106fa87b21ab01a51f87ed79587b
Yan Yi Goh <yanyi@users.noreply.github.com>
2025-08-21 20:40:10 +0800

    brew.sh: Clean up
    Clean up unused (like rarely used) casks and formulae.
    
    Also, Dozer is not supported on Homebrew any longer. Trying out Ice (jordanbaird-ice).

--- brew.sh
+++ brew.sh
@@ -6,14 +6,14 @@
 # brew-cask daily
 brew install --cask alfred
 brew install --cask docker
-brew install --cask dozer
 brew install --cask dropbox
 brew install --cask firefox
 brew install --cask iterm2
+brew install --cask jordanbaird-ice
 brew install --cask keepingyouawake
 brew install --cask maccy
+brew install --cask obsidian
 brew install --cask rectangle
-brew install --cask sourcetree
 brew install --cask spotify
 brew install --cask telegram
 brew install --cask the-unarchiver
@@ -21,14 +21,11 @@ brew install --cask visual-studio-code
 brew install --cask whatsapp
 
 # brew-cask less often
-brew install --cask aerial
 brew install --cask appcleaner
 brew install --cask cryptomator
 brew install --cask discord
-brew install --cask dnscrypt
 brew install --cask freefilesync
 brew install --cask google-chrome
-brew install --cask google-cloud-sdk
 brew install --cask handbrake
 brew install --cask iina
 brew install --cask imageoptim
@@ -37,20 +34,14 @@ brew install --cask libreoffice
 brew install --cask medis
 brew install --cask namechanger
 brew install --cask signal
-brew install --cask slack
-brew install --cask time-lapse-assembler
 brew install --cask transmission
-brew install --cask veracrypt
-brew install --cask virtualbox
 
 # Install useful binaries
 brew install autojump
-brew tap wagoodman/dive && brew install dive
 brew install fzf
 brew install git
 brew install git-crypt
 brew install git-delta
-brew install git-flow
 brew install htop
 brew install just
 brew install openssl
@@ -64,10 +55,6 @@ brew install watch
 brew install wget
 brew install yarn
 brew install youtube-dl
-
-# brew taps
-brew tap buo/cask-upgrade # https://github.com/buo/homebrew-cask-upgrade
-brew tap homebrew/cask-fonts
 
 # Remove outdated versions from the cellar
 brew cleanup
